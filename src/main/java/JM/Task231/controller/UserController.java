0e43e2dcd40bf1d83f8cc6711a893d0ea8928d8c
BuildTools <unconfigured@null.spigotmc.org>
2020-09-13 13:15:54 +0300

    ALL DONE Yeah!

--- src/main/java/JM/Task231/controller/UserController.java
+++ src/main/java/JM/Task231/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -78,7 +79,9 @@ public class UserController {
 
     @RequestMapping(value = "/admin/add", method = RequestMethod.GET)
     public ModelAndView addPage() {
+        User user = new User();
         ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("user", user);
         modelAndView.setViewName("addPage");
         return modelAndView;
     }
